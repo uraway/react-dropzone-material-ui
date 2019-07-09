208aac43088781854506d911c698f9ae9787025d
uraway <masato.uraway@gmail.com>
2019-07-09 22:29:34 +0800

    Updates

---
+++ precache-manifest.094fd3fed43f94c4b1ceb0dd94f89cb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8074851b66d2db2691779001a8903e",
+    "url": "/react-dropzone-material-ui/index.html"
+  },
+  {
+    "revision": "0e9e101d9be24d7a8eff",
+    "url": "/react-dropzone-material-ui/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "263ce9ad78b14d0ff1d2",
+    "url": "/react-dropzone-material-ui/static/js/2.d48822b5.chunk.js"
+  },
+  {
+    "revision": "0e9e101d9be24d7a8eff",
+    "url": "/react-dropzone-material-ui/static/js/main.34e4d5d0.chunk.js"
+  },
+  {
+    "revision": "f21c6be5d790276a630c",
+    "url": "/react-dropzone-material-ui/static/js/runtime~main.c4435efc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-dropzone-material-ui/precache-manifest.094fd3fed43f94c4b1ceb0dd94f89cb4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-dropzone-material-ui/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
